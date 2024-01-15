86adb8730db8cdfa705143b00370e2ed06638f33
strelkovsky <strelkin7@gmail.com>
2024-01-15 13:23:29 +0300

    make WinQuake project compilable

--- WinQuake/sys_win.c
+++ WinQuake/sys_win.c
@@ -270,7 +270,7 @@ void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length)
 }
 
 
-#ifndef _M_IX86
+//#ifndef _M_IX86
 
 void Sys_SetFPCW (void)
 {
@@ -288,7 +288,7 @@ void MaskExceptions (void)
 {
 }
 
-#endif
+//#endif
 
 /*
 ================

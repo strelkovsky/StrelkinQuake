c6ca143d15724a05ab09478b4d02d1393459ee6b
strelkovsky <strelkin7@gmail.com>
2024-01-15 13:20:16 +0300

    increase console msg length

--- WinQuake/console.c
+++ WinQuake/console.c
@@ -372,7 +372,8 @@ Con_Printf
 Handles cursor positioning, line wrapping, etc
 ================
 */
-#define	MAXPRINTMSG	4096
+// @Strelkin: increased from 4096 to 16384
+#define	MAXPRINTMSG	16384
 // FIXME: make a buffer size safe vsprintf?
 void Con_Printf (char *fmt, ...)
 {

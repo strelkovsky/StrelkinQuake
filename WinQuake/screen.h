467cb382e8e27df1684b332059308e4ab4a4fe13
strelkovsky <strelkin7@gmail.com>
2025-05-07 15:02:55 +0300

    cleanup

--- WinQuake/screen.h
+++ WinQuake/screen.h
@@ -23,9 +23,6 @@ void SCR_Init (void);
 
 void SCR_UpdateScreen (void);
 
-
-void SCR_SizeUp (void);
-void SCR_SizeDown (void);
 void SCR_BringDownConsole (void);
 void SCR_CenterPrint (char *str);
 
